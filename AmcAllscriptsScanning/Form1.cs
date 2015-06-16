94d447c888a8ea5271da9c953e4d4cdc03ea5b75
wralex <wralex@gmail.com>
2015-06-16 16:09:17 -0400

    Commit

--- AmcAllscriptsScanning/Form1.cs
+++ AmcAllscriptsScanning/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Accessibility;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 using WIAUtility;
@@ -13,6 +16,8 @@ namespace AmcAllscriptsScanning
 {
     public partial class Form1 : Form
     {
+        private const string ieMainTitle = "Eclipse PPM";
+
         public Form1()
         {
             InitializeComponent();
@@ -25,8 +30,9 @@ namespace AmcAllscriptsScanning
 
         private void button2_Click(object sender, EventArgs e)
         {
-            WebCam cam = new WebCam();
-            String[] s = cam.GetConnectedCameras();
+            IEAccessible ieEclipse = new IEAccessible(@"https://eod1.eclipseppm.com/");
         }
+
+
     }
 }
---
+++ AmcAllscriptsScanning/User32API.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace AmcAllscriptsScanning
+{
+    internal static partial class User32API
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        private static extern bool BringWindowToTop(IntPtr hWnd);
+
+    }
+}
